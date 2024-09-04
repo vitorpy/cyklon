0ed211c586a059a940005971c5d2ea66c63e9a30
Vitor Py Braga <12871+vitorpy@users.noreply.github.com>
2024-09-04 17:40:40 +0200

    chore: trying to get this to build

--- anchor/programs/cyklon/src/instructions/initialize_pool.rs
+++ anchor/programs/cyklon/src/instructions/initialize_pool.rs
@@ -1,27 +1,29 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::Mint;
+use anchor_spl::token_interface::Mint;
 use crate::state::Pool;
-
-pub fn initialize_pool(
-    ctx: Context<InitializePool>,
-    tick_spacing: u16,
-    initial_sqrt_price: u128,
-) -> Result<()> {
-    let pool = &mut ctx.accounts.pool;
-    pool.token_mint_0 = ctx.accounts.token_mint_0.key();
-    pool.token_mint_1 = ctx.accounts.token_mint_1.key();
-    pool.tick_spacing = tick_spacing;
-    pool.sqrt_price = initial_sqrt_price;
-    Ok(())
-}
 
 #[derive(Accounts)]
 pub struct InitializePool<'info> {
     #[account(init, payer = payer, space = 8 + 32 + 32 + 2 + 16)]
     pub pool: Account<'info, Pool>,
-    pub token_mint_0: Account<'info, Mint>,
-    pub token_mint_1: Account<'info, Mint>,
+    pub token_mint_0: Box<InterfaceAccount<'info, Mint>>,
+    pub token_mint_1: Box<InterfaceAccount<'info, Mint>>,
     #[account(mut)]
     pub payer: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+impl<'info> InitializePool<'info> {
+    pub fn initialize_pool(
+        &mut self,
+        tick_spacing: u16,
+    initial_sqrt_price: u128,
+) -> Result<()> {
+        let pool = &mut self.pool;
+        pool.token_mint_0 = self.token_mint_0.key();
+        pool.token_mint_1 = self.token_mint_1.key();
+        pool.tick_spacing = tick_spacing;
+        pool.sqrt_price = initial_sqrt_price;
+        Ok(())
+    }
 }
