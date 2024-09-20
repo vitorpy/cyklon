fdc15e90a016f740e69dd2e723ccbbf73dff6700
Vitor Py Braga <12871+vitorpy@users.noreply.github.com>
2024-09-20 23:04:21 +0200

    feat: minimal visual

--- web/components/dashboard/dashboard-feature.tsx
+++ web/components/dashboard/dashboard-feature.tsx
@@ -6,8 +6,8 @@ import { SolanaSwapComponent } from '../solana-swapper/solana-swap';
 export default function DashboardFeature() {
   return (
     <div>
-      <AppHero title="Blackpool Swap" subtitle="Trade privately." />
-      <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-9">
+      <AppHero title="Trade Privately. Now." subtitle="Blackpool is a privacy-preserving trading platform built on Solana." />
+      <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-9 mt-[50px]">
         <SolanaSwapComponent />
       </div>
     </div>
