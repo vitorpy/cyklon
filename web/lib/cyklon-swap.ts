54636285dbd283583bd718dc4ed0b9f112dcafc8
Vitor Py Braga <12871+vitorpy@users.noreply.github.com>
2024-10-08 00:15:02 +0200

    fix: build

--- web/lib/cyklon-swap.ts
+++ web/lib/cyklon-swap.ts
@@ -1,4 +1,4 @@
-'use server'
+'use client'
 
 import { AnchorProvider, utils } from '@coral-xyz/anchor';
 import { PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
@@ -6,67 +6,13 @@ import { getCyklonProgram, getCyklonProgramId } from '@blackpool/anchor';
 import { useAnchorProvider } from '../components/solana/solana-provider';
 import { useCluster } from '../components/cluster/cluster-data-access';
 import { getAssociatedTokenAddress } from '@solana/spl-token';
-import * as snarkjs from 'snarkjs';
-import * as path from 'path';
-// @ts-expect-error ffjavascript is not typed.
-import { buildBn128, utils as ffUtils } from 'ffjavascript';
-import { g1Uncompressed, negateAndSerializeG1, g2Uncompressed, to32ByteBuffer } from "@blackpool/anchor";
 import { TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token';
-
-const { unstringifyBigInts } = ffUtils;
+import { generateProof } from './prepare-proof';
 
 export interface SwapResult {
   success: boolean;
   transaction?: Transaction;
   error?: string;
-}
-
-async function generateProof(
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  privateInputs: any,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  publicInputs: any
-): Promise<{ proofA: Uint8Array, proofB: Uint8Array, proofC: Uint8Array, publicSignals: Uint8Array[] }> {
-  console.log("Generating proof for inputs:", { privateInputs, publicInputs });
-
-  const wasmPath = path.join(__dirname, "../../swap_js", "swap.wasm");
-  const zkeyPath = path.join(__dirname, "../../", "swap_final.zkey");
-
-  const input = {
-    privateAmount: privateInputs.privateAmount.toString(),
-    privateMinReceived: privateInputs.privateMinReceived.toString(),
-    publicBalanceX: publicInputs.publicBalanceX.toString(),
-    publicBalanceY: publicInputs.publicBalanceY.toString(),
-    isSwapXtoY: publicInputs.isSwapXtoY.toString(),
-    totalLiquidity: publicInputs.totalLiquidity.toString()
-  };
-
-  const { proof, publicSignals } = await snarkjs.groth16.fullProve(input, wasmPath, zkeyPath);
-
-  console.log("Original proof:", JSON.stringify(proof, null, 2));
-  console.log("Public signals:", JSON.stringify(publicSignals, null, 2));
-
-  const curve = await buildBn128();
-  const proofProc = unstringifyBigInts(proof);
-  const publicSignalsUnstrigified = unstringifyBigInts(publicSignals);
-
-  let proofA = g1Uncompressed(curve, proofProc.pi_a);
-  proofA = await negateAndSerializeG1(curve, proofA);
-
-  const proofB = g2Uncompressed(curve, proofProc.pi_b);
-  const proofC = g1Uncompressed(curve, proofProc.pi_c);
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const formattedPublicSignals = publicSignalsUnstrigified.map((signal: any) => {
-    return to32ByteBuffer(BigInt(signal));
-  });
-
-  return { 
-    proofA: new Uint8Array(proofA), 
-    proofB: new Uint8Array(proofB), 
-    proofC: new Uint8Array(proofC), 
-    publicSignals: formattedPublicSignals 
-  };
 }
 
 export async function prepareConfidentialSwap(
@@ -79,8 +25,6 @@ export async function prepareConfidentialSwap(
   sourceTokenProgram: string,
   destTokenProgram: string
 ): Promise<SwapResult> {
-  'use server'
-  
   try {
     const program = getCyklonProgram(provider);
     const payer = provider.wallet;
